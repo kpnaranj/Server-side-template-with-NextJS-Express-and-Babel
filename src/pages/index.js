f72526140efe116b6eae7f7ea5753bc3af99b3fb
Kevin Narsh <kpnaranj@uwaterloo.ca>
2020-11-22 22:52:42 -0500

    Revert "Adding an example box"
    This reverts commit 21ea0ed7a7daead019d6dc8a2259b7d9af88d4df.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import Head from "next/head";
 import Layout from "../components/Layout";
-import ExampleForm from "../components/ExampleForm";
+
 export default function Home() {
   return (
     <Fragment>
@@ -10,8 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <main className="text-center">This is the example box</main>
-        <ExampleForm id={1} />
+        <main className="text-center">This is main</main>
         <footer className="text-center">This is footer</footer>
       </Layout>
     </Fragment>
